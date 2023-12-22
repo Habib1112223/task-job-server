6432e0a5aa7111aa107e5b328ef8f2e43e3fc867
Habibur1112223 <webdevelopment.habib2023@gmail.com>
2023-12-22 18:34:21 +0600

    local server side is running the code

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express =require("express");
 const cors = require ('cors')
 const app = express();
+const port = process.env.PORT||5000;
+
+app.get('/',(req,res) =>{
+      res.send("job tasking is running for local")
+})
+
+app.listen(port,()=>{
+      console.log(`Tasking job is running ${port}`);
+})
